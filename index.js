dd721f1485084f07a508fceefc91ca1abc868c93
Alex Martinez <alexmartinez0071234@gmail.com>
2020-01-28 17:26:28 -0500

    first 6 questions done

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ function processSum(numberList,callback) {
  * should return 1000.
 */
 function processProduct(num1,num2,callback) {
-  /* CODE HERE */
+  return callback(num1 * num2);
 }
 
 /**
@@ -139,8 +139,9 @@ function processProduct(num1,num2,callback) {
  * "lady gaga" and `['foo', 'bar']` and `(bool) => bool ? 'nice!' : 'sad'`,
  * should return "sad".
 */
-function processContains(/* CODE HERE */) {
-  /* CODE HERE */
+function processContains(item,list,callback) {
+
+  return callback(list.includes(item));
 }
 
 /**
@@ -184,8 +185,10 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners){
+  runners.forEach(function(element){
+    return console.log(`${element.last_name},${element.first_name}`);
+  });
 }
 
 /**
